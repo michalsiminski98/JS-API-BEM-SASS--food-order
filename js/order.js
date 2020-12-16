da956d02f3709878312aecfb83ada14995c64048
Michal Siminski <michal.siminski3@gmail.com>
2020-12-16 09:25:18 +0100

    add max order - 5

--- js/order.js
+++ js/order.js
@@ -3,23 +3,27 @@ const wrapper = document.querySelector('.favourites-wrapper__box');
 
 
 const addOrder = () => {
-    alert("Dodano do zamówienia");
-    // create order wrapper
-    let order = document.createElement('div');
-    let image = document.createElement('div');
-    let title = document.createElement('p');
+    if (wrapper.children.length < 5) {
+        alert("Dodano do zamówienia");
+        // create order wrapper
+        let order = document.createElement('div');
+        let image = document.createElement('div');
+        let title = document.createElement('p');
 
-    order.classList.add('favourites-wrapper__order');
-    image.classList.add('favourites-wrapper__photo');
-    title.classList.add('favourites-wrapper__title');
+        order.classList.add('favourites-wrapper__order');
+        image.classList.add('favourites-wrapper__photo');
+        title.classList.add('favourites-wrapper__title');
 
-    wrapper.appendChild(order);
-    order.appendChild(image);
-    order.appendChild(title);
+        wrapper.appendChild(order);
+        order.appendChild(image);
+        order.appendChild(title);
 
-    // add dish to order
-    image.style.backgroundImage = `${document.querySelector('.meal__img').style.backgroundImage}`;
-    title.innerHTML = `${document.querySelector('.meal__name').innerHTML}`;
+        // add dish to order
+        image.style.backgroundImage = `${document.querySelector('.meal__img').style.backgroundImage}`;
+        title.innerHTML = `${document.querySelector('.meal__name').innerHTML}`;
+    } else {
+        alert('You have too much dishes in order')
+    }
 }
 
 
